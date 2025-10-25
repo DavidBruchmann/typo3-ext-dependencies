8a44d354f6ab2883439f3c654c94b0b073e0c063
David Bruchmann <david.bruchmann@gmail.com>
2025-10-25 19:13:29 +0700

    [Task] minor adjustments in composer.json

--- ext_tables.php
+++ ext_tables.php
@@ -2,22 +2,24 @@
 
 defined('TYPO3') or die();
 
+use Wdb\Dependencies\Controller\DependencyModuleController;
+
 call_user_func(function () {
-    
-    /*
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Dependencies', // Extension Name in CamelCase
-        'tools', // the main module
-        'dependencies', // Submodule key
-        'bottom', // Position
-        [
-            \Wdb\Dependencies\Controller\DependencyModuleController::class => 'index'
-        ],
-        [
-            'access' => 'admin',
-            'icon' => 'EXT:dependencies/Resources/Public/Icons/Module.svg',
-            'labels' => 'LLL:EXT:dependencies/Resources/Private/Language/locallang_mod.xlf',
-        ]
-    );
-    */
+    // Only register for TYPO3 < v12
+    if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 12000000) {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'Dependencies', // Extension Name in CamelCase
+            'tools', // the main module
+            'dependencies', // Submodule key
+            'bottom', // Position
+            [
+                DependencyModuleController::class => 'index'
+            ],
+            [
+                'access' => 'admin',
+                'icon' => 'EXT:dependencies/Resources/Public/Icons/Module.svg',
+                'labels' => 'LLL:EXT:dependencies/Resources/Private/Language/locallang_mod.xlf',
+            ]
+        );
+    }
 });
